86e0dacdb1c8e793cc47a62f89d71f4f550cc2d0
byoso <emailvinz@gmail.com>
2022-08-15 21:01:15 +0200

    all book's urls aquired, TODO: get each book's informations

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import requests
 from bs4 import BeautifulSoup as BS
 
 URL = "http://books.toscrape.com/"
+URL_CATALOG = "http://books.toscrape.com/catalogue/"
+
+
+def display_categories(categories):
+    """Display categories"""
+    for i,cat in enumerate(categories):
+        print(f"{i+1:<3} {cat['name']:<20}: {cat['index']}")
 
 
 def get_soup(url="http://books.toscrape.com/"):
@@ -41,29 +48,56 @@ def csv_write(datas=None):
 def get_categories():
     """Get all the categories and their urls, returns a dict"""
     soup = get_soup()
-    result = soup.findAll("li", class_="")
-    categories = {}
+    result = soup.find_all("li", class_="")
+    categories = []
     for i in result[2:]:  # excludes 'home' and 'books'
+        category = {}
         url = urllib.parse.urljoin(URL, i.a['href'])
-        categories[i.a.get_text().strip()] = url
+        category['name'] = i.a.get_text().strip()
+        category['index'] = url
+        categories.append(category)
     return categories
 
 
-def get_books(url="http://books.toscrape.com/catalogue/category/books/fantasy_19/index.html"):
+def get_books(
+    url=(
+        "http://books.toscrape.com/catalogue/"
+        "category/books/fantasy_19/index.html"
+        ),
+    books=None,
+):
     """get all books in a category"""
+    if books is None:
+        books = []
     soup = get_soup(url)
-    books = soup.find_all(class_="product_pod")
-    print(books)
+    next = soup.find(class_="next")
+    result = soup.find_all(class_="product_pod")
+    for elem in result:
+        page = ("/").join(elem.a['href'].split("/")[-2:])
+        books.append(urllib.parse.urljoin(URL_CATALOG, page))
+    if next is not None:
+        next_url = urllib.parse.urljoin(url, next.a['href'])
+        books = get_books(next_url, books)
+    # print(len(books))
+    return books
 
 
-def main():
+def get_all():
     categories = get_categories()
-    #### debug
-    for i, cat in enumerate(categories):
-        print(f"{i+1:<3} {cat:<20}: {categories[cat]}")
-    ####
+    for cat in categories:
+        cat['books'] = get_books(cat['index'])
+    return categories
+
+
+def main(verbose=False):
+    categories = get_categories()
+    if verbose:
+        display_categories(categories)
 
 
 if __name__ == "__main__":
-    # main()
-    get_books()
+    try:
+        main(True)
+        # get_books()
+    except KeyboardInterrupt:
+        print("\n--END--")
